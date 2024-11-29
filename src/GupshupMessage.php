1063653c1bbc234454ea284c3c85e92f0db9caca
Vivek Dhumal <vivekdhumalmail@gmail.com>
2024-11-29 19:41:30 +0530

    clean up query params

--- src/GupshupMessage.php
+++ src/GupshupMessage.php
@@ -64,23 +64,23 @@ class GupshupMessage
                 'base_uri' => 'https://enterprise.smsgupshup.com',
                 'verify' => $this->sslVerify,
             ]);
-            
-            $response = $client->get('/GatewayAPI/rest', [
-                'query' => [
-                    'method' => 'SendMessage',
-                    'send_to' => $this->to,
-                    'msg' => $this->message,
-                    'msg_type' => 'TEXT',
-                    'userid' => $this->userid,
-                    'auth_scheme' => 'plain',
-                    'password' => $this->password,
-                    'v' => '1.1',
-                    'format' => 'JSON',
-                    'mask' => $this->mask,
-                    'principalEntityId' => $this->entityId,
-                    'dltTemplateId' => $this->templateId,
-                ]
+
+            $queryParams = array_filter([
+                'method' => 'SendMessage',
+                'send_to' => $this->to,
+                'msg' => $this->message,
+                'msg_type' => 'TEXT',
+                'userid' => $this->userid,
+                'auth_scheme' => 'plain',
+                'password' => $this->password,
+                'v' => '1.1',
+                'format' => 'JSON',
+                'mask' => $this->mask,
+                'principalEntityId' => $this->entityId,
+                'dltTemplateId' => $this->templateId,
             ]);
+            
+            $response = $client->get('/GatewayAPI/rest', ['query' => $queryParams]);
             
             if($response->getStatusCode() >= 200) {
                 $data = json_decode($response->getBody()->getContents(), true);
